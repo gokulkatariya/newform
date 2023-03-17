aad7cf01077a43156c39975ab4a47de39723344c
gokulkatariya <gk2800598@gmail.com>
2023-03-17 08:30:23 +0530

    gokul

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./a.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="sub-main">
+          <div>
+            <div className="imgs">
+              <div className="container-image">
+                <img src={profile} alt="profile" className="profile" />
+              </div>
+            </div>
+            <div>
+              <h1>Signup Page</h1>
+              <div>
+                <input type="text" placeholder="User name" className="name"/>
+              </div>
+              <div className="second-input">
+                <input type="email" placeholder="Email id" className="name"/>
+              </div> 
+              <div className="third-input">
+                <input type="password" placeholder="Password" className="name"/>
+              </div>
+              <div className="four-input">
+                <input type="contact" placeholder="contact" className="name"/>
+              </div>
+              <div className="five-input">
+                <input type="address" placeholder="address" className="name"/>
+              </div>
+              <div className="login-button">
+                <button>Signup</button>
+              </div>
+
+              <p className="link">
+                <a href="/">Forgot password </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
