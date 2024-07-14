12f63c593b5e98d3e137db281748e6883b8f4fc7
gokulkatariya <gk2800598@gmail.com>
2024-07-14 15:07:05 +0530

    commit

--- src/components/form.js
+++ src/components/form.js
@@ -3,23 +3,22 @@ import React from 'react'
 import { useState } from 'react'
 
 function Form() {
-    const [User_name, setText] = useState("");
-    const [Email, setEmail] = useState("");
-    const [Contect_No, setNumber] = useState("");
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("");
     const [address, setAddress] = useState("");
 
     const handleSubmit = () => {
-        fetch('http://127.0.0.1:8000/sign_up/', {
+        fetch('', {
             method: 'POST',
             body: JSON.stringify({
-                User_Name: User_name,
-                Email: Email,
-                Contect_No: Contect_No,
-                address: address,
+                User: user,
+                Email: email,
+                Number: number,
+                Address: address,
             }),
             headers: {
                 'Content-type': 'application/json;',
-                
             },
         })
             .then((response) => response.json())
@@ -31,36 +30,24 @@ function Form() {
         <div className="main" >
             <div className="sub-main">
                 <div>
-                    {/* <div className="imgs">
-                        <div className="container-image">
-                            <img src={profile} alt="profile" className="profile" />
-                        </div> */}
-                    {/* </div> */}
                     <div>
                         <h1>Signup </h1>
-
                         <div>
-                            <input type="text" placeholder="username" className="name" value={User_name}onChange={(e) => setText(e.target.value)}/>
-
+                            <input type="text" placeholder="username" value={user} onChange={(e) => setUser(e.target.value)} />
                         </div>
                         <div className="second-input">
-                            <input type="email" placeholder="email" className="name" value={Email}onChange={(e) => setEmail(e.target.value)}
-                                />
-                                {/* # onChange={(e) => setEmail(e.target.value)}  */}
-
+                            <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="third-input">
-
-                            <input type="number" placeholder="number" className="name" value={Contect_No} onChange={(e) => setNumber(e.target.value)}/>
+                            <input type="contact" placeholder="number" value={number} onChange={(e) => setNumber(e.target.value)} />
                         </div>
-                       
                         <div className="five-input">
-                            <input type="massege" placeholder="address" className="name" value={address}onChange={(e) => setAddress(e.target.value)} />
+                            <input type="massege" placeholder="address" value={address} onChange={(e) => setAddress(e.target.value)} />
                         </div>
                         <div className="signup-button">
                             <button type="submit" onClick={handleSubmit}>Signup</button>
                         </div>
-
                         <p className="link">
                             <a href="/">Forgot password </a>
                         </p>
